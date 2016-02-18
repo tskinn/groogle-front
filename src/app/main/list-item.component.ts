3bbaebfdbe0f69a733db01b367325a1944fea527
Taylor Skinner <tskinner@kyanicorp.com>
2016-02-18 13:00:35 -0700

    created css and html files for component

--- src/app/main/list-item.component.ts
+++ src/app/main/list-item.component.ts
@@ -3,20 +3,8 @@ import {Result} from './result';
 
 @Component({
 	 selector: 'list-item',
-	 template: `
-<div *ngIf="result" class="page_group">
-Site: {{ result.site }} <br>
-Rank: {{ result.rank }} <br>
-
-  <ul>
-    <li *ngFor="#index of result.indices" class="context">
-      <div>{{ result.page.slice(index - 30, index + 30) }}</div>
-    </li>
-  </ul>
-</div>
-`,
-	 styles: ['.page_group {	 border: dotted;	 background-color: red;}']
-
+	 styleUrls: ['app/main/list-item.css'],
+	 templateUrl: 'app/main/list-item.html' 
 })
 export class ListItem {
 	 @Input() result: Result;
